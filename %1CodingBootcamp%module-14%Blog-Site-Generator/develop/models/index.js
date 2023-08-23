f0b36465f6c5794912173fd40dace885e0e8ea09
CSillus <71670415+csills1416@users.noreply.github.com>
2023-08-23 08:22:03 -0400

    added server.js and moved site.css to the right folder

--- %1CodingBootcamp%module-14%Blog-Site-Generator/develop/models/index.js
+++ %1CodingBootcamp%module-14%Blog-Site-Generator/develop/models/index.js
@@ -1,3 +1,11 @@
-const User = require('../models/User');
+const User = require('./User');
+const Blog = require('./Blog');
 
-module.exports = {User}
+User.hasMany(Blog, {
+  foreignKey: 'user_id',
+});
+Blog.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+module.exports = { User, Blog };
