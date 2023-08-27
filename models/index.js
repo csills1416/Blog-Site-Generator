abd7372b20444f54a2d82af3040c3a9879f3b677
CSillus <71670415+csills1416@users.noreply.github.com>
2023-08-27 13:11:14 -0400

    tinkered with index.js

--- models/index.js
+++ models/index.js
@@ -1,32 +1,33 @@
 const sequelize = require('../config/connection');
+console.log('sequelize instance:', sequelize);
 
 const User = require('./User');
-const Post = require('./Post');
-const Comment = require('./Comment');
+const { Post } = require('./Post');
+const { Comment } = require('./Comment');
 
 // Set up associations
 User.hasMany(Post, {
-    onDelete: 'cascade',
-    onUpdate: 'cascade',
-    foreignKey: 'user_id'
+  onDelete: 'cascade',
+  onUpdate: 'cascade',
+  foreignKey: 'user_id',
 });
 
 Post.belongsTo(User, {
-    onDelete: 'cascade',
-    onUpdate: 'cascade',
-    foreignKey: 'user_id'
+  onDelete: 'cascade',
+  onUpdate: 'cascade',
+  foreignKey: 'user_id',
 });
 
 Post.hasMany(Comment, {
-    onDelete: 'cascade',
-    onUpdate: 'cascade',
-    foreignKey: 'post_id'
+  onDelete: 'cascade',
+  onUpdate: 'cascade',
+  foreignKey: 'post_id',
 });
 
 Comment.belongsTo(Post, {
-    onDelete: 'cascade',
-    onUpdate: 'cascade',
-    foreignKey: 'post_id'
+  onDelete: 'cascade',
+  onUpdate: 'cascade',
+  foreignKey: 'post_id',
 });
 
 module.exports = { User, Post, Comment };
